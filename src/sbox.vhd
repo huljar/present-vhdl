ba473adf8ee6586001b501646a1cbed3e3990e93
Julian Harttung <julian.harttung@posteo.de>
2017-02-24 19:33:13 +0100

    Initial commit; Implemented SBox and PRESENT substitution layer

---
+++ src/sbox.vhd
@@ -0,0 +1,34 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity sbox is
+    port(data_in:  in std_logic_vector(3 downto 0);
+         data_out: out std_logic_vector(3 downto 0)
+    );
+end sbox;
+
+architecture behavioral of sbox is
+    begin
+        process(data_in)
+        begin
+            case data_in is
+                when x"0" => data_out <= x"C";
+                when x"1" => data_out <= x"5";
+                when x"2" => data_out <= x"6";
+                when x"3" => data_out <= x"B";
+                when x"4" => data_out <= x"9";
+                when x"5" => data_out <= x"0";
+                when x"6" => data_out <= x"A";
+                when x"7" => data_out <= x"D";
+                when x"8" => data_out <= x"3";
+                when x"9" => data_out <= x"E";
+                when x"A" => data_out <= x"F";
+                when x"B" => data_out <= x"8";
+                when x"C" => data_out <= x"4";
+                when x"D" => data_out <= x"7";
+                when x"E" => data_out <= x"1";
+                when x"F" => data_out <= x"2";
+                when others => null; -- GHDL complains without this statement
+            end case;
+        end process;
+    end behavioral;
---
+++ src/sub_layer.vhd
@@ -0,0 +1,24 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity sub_layer is
+    port(data_in:  in std_logic_vector(63 downto 0);
+         data_out: out std_logic_vector(63 downto 0)
+    );
+end sub_layer;
+
+architecture behavioral of sub_layer is
+    component sbox
+        port(sbox_in:  in std_logic_vector(3 downto 0);
+             sbox_out: out std_logic_vector(3 downto 0)
+        );
+    end component;
+
+    begin
+        GEN_SBOXES: for i in 15 downto 0 generate
+            SX: sbox port map(
+                sbox_in => data_in(4*i+3 downto 4*i),
+                sbox_out => data_out(4*i+3 downto 4*i)
+            );
+        end generate;
+    end;
